c7941ecd7974d2f87ee182714144c1507acf2626
Raymond <raymond.zhang@alphaliongroup.com>
2020-09-23 18:03:18 +0800

    change swagger msg to hins mall

--- foodie-dev-api/src/main/java/com/hins/config/Swagger2.java
+++ foodie-dev-api/src/main/java/com/hins/config/Swagger2.java
@@ -32,13 +32,13 @@ public class Swagger2 {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("天天吃货 电商平台接口api")        // 文档页标题
-                .contact(new Contact("imooc",
-                        "https://www.imooc.com",
-                        "abc@imooc.com"))        // 联系人信息
-                .description("专为天天吃货提供的api文档")  // 详细信息
+                .title("Hins Mall")        // 文档页标题
+                .contact(new Contact("hins",
+                        "https://www.hins.com",
+                        "abc@hins.com"))        // 联系人信息
+                .description("专为Hins Mall提供的api文档")  // 详细信息
                 .version("1.0.1")   // 文档版本号
-                .termsOfServiceUrl("https://www.imooc.com") // 网站地址
+                .termsOfServiceUrl("https://www.hins.com") // 网站地址
                 .build();
     }
 
