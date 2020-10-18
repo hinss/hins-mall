46fafa5af295fcdb70a038161f768da114bcf86d
wyman <277439189@qq.com>
2020-10-18 17:04:01 +0800

    add 部署环境配置文件修改

--- foodie-dev-api/src/main/java/com/hins/config/WebMvcConfig.java
+++ foodie-dev-api/src/main/java/com/hins/config/WebMvcConfig.java
@@ -19,7 +19,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/META-INF/resources/")  // 映射swagger2
-                .addResourceLocations("file:/Applications/hins/file-server/"); //映射本地静态资源
+                .addResourceLocations("file:/workspaces/images/foodie/faces"); //映射本地静态资源
     }
 
     @Bean
