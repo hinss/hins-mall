ea4b88823af6ce1b03fac56c637f9aaf51d3101c
wyman <277439189@qq.com>
2020-11-08 12:39:06 +0800

    新增用户toekn 鉴权拦截功能

--- foodie-dev-api/src/main/java/com/hins/config/WebMvcConfig.java
+++ foodie-dev-api/src/main/java/com/hins/config/WebMvcConfig.java
@@ -1,9 +1,11 @@
 package com.hins.config;
 
+import com.hins.interceptor.UserTokenInterceptor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -28,4 +30,36 @@ public class WebMvcConfig implements WebMvcConfigurer {
     }
 
 
+    @Bean
+    public UserTokenInterceptor userTokenInterceptor(){
+        return new UserTokenInterceptor();
+    }
+
+    /**
+     * 注册拦截器
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(userTokenInterceptor())
+                .addPathPatterns("/hello")
+                .addPathPatterns("/redis/*")
+                .addPathPatterns("/shopcart/add")
+                .addPathPatterns("/shopcart/del")
+                .addPathPatterns("/address/list")
+                .addPathPatterns("/address/add")
+                .addPathPatterns("/address/update")
+                .addPathPatterns("/address/setDefalut")
+                .addPathPatterns("/address/delete")
+                .addPathPatterns("/orders/*")
+                .addPathPatterns("/center/*")
+                .addPathPatterns("/userInfo/*")
+                .addPathPatterns("/myorders/*")
+                .addPathPatterns("/mycomments/*")
+                .excludePathPatterns("/myorders/deliver")
+                .excludePathPatterns("/orders/notifyMerchantOrderPaid");
+
+        WebMvcConfigurer.super.addInterceptors(registry);
+    }
 }
---
+++ foodie-dev-api/src/main/java/com/hins/interceptor/UserTokenInterceptor.java
@@ -0,0 +1,100 @@
+package com.hins.interceptor;
+
+import com.hins.utils.JSONResult;
+import com.hins.utils.JsonUtils;
+import com.hins.utils.RedisOperator;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * @Description: 用户token拦截器
+ * @Author:Wyman
+ * @Date: 2020-11-08
+ */
+public class UserTokenInterceptor implements HandlerInterceptor {
+
+    public static final String REDIS_USER_TOKEN = "redis_user_token";
+
+    @Autowired
+    private RedisOperator redisOperator;
+
+    /**
+     * 拦截请求，在访问controller调用之前
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String userId = request.getHeader("headerUserId");
+        String userToken = request.getHeader("headerUserToken");
+
+        if(StringUtils.isNotBlank(userId) && StringUtils.isNotBlank(userToken)){
+            String uniqueToken = redisOperator.get(REDIS_USER_TOKEN + ":" + userId);
+            if(StringUtils.isBlank(uniqueToken)){
+                returnErrorResponse(response, JSONResult.errorMsg("请登录"));
+                return false;
+            }else{
+
+                if(!userToken.equalsIgnoreCase(uniqueToken)){
+                    returnErrorResponse(response, JSONResult.errorMsg("账号在异地登录"));
+                    return false;
+                }
+            }
+        }else{
+            returnErrorResponse(response, JSONResult.errorMsg("请登录"));
+            return false;
+        }
+        /**
+         * false: 请求被拦截，被驳回，验证出现问题
+         * true: 请求再经过验证校验以后是OK的，是可以放行的
+         */
+        return true;
+    }
+
+    public void returnErrorResponse(HttpServletResponse response,
+                                    JSONResult jsonResult){
+
+        OutputStream out = null;
+        try {
+            response.setCharacterEncoding("utf-8");
+            response.setContentType("text/json");
+            out = response.getOutputStream();
+            out.write(JsonUtils.objectToJson(jsonResult).getBytes());
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(out != null){
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    /**
+     * 请求访问controller之后，渲染视图之前
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    /**
+     * 请求访问controller之后，渲染视图之后
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
